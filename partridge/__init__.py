501e9ff4907acc91637737279a404071a6e4314f
Danny Whalen <daniel.r.whalen@gmail.com>
2017-09-10 22:57:57 -0700

    Export top-level readers for operating directly on gtfs path

--- partridge/__init__.py
+++ partridge/__init__.py
@@ -1,1 +1,13 @@
 from partridge.gtfs import feed, raw_feed
+
+
+def read_service_ids_by_date(path):
+	return feed(path).service_ids_by_date
+
+
+def read_dates_by_service_ids(path):
+	return feed(path).dates_by_service_ids
+
+
+def read_trip_counts_by_date(path):
+	return feed(path).trip_counts_by_date
