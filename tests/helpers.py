2e5a8932594620ee66d08586492b3a00a0b61eca
Danny Whalen <daniel.r.whalen@gmail.com>
2018-03-08 21:46:18 -0800

    Refactor helpers.zip_file
    `shutil.make_archive` with preserve nested folders
    which is needed in upcoming commits.

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,26 +1,18 @@
 import os
-from os import path
-import zipfile
+import shutil
 
 
 def fixture(filename):
     """
     Get the handle / path to the test data folder.
     """
-    return path.join(path.dirname(__file__), 'fixtures', filename)
+    return os.path.join(os.path.dirname(__file__), 'fixtures', filename)
 
 
 def zip_file(scenario):
     """
     Get the file handle / path to the zip file.
     """
-    basedir = path.dirname(__file__)
-    scenario_dir = path.join(basedir, 'fixtures', scenario)
-    scenario_file = path.join(basedir, 'fixtures', scenario + '.zip')
-
-    with zipfile.ZipFile(scenario_file, 'w') as zipf:
-        for root, dirs, files in os.walk(scenario_dir):
-            for gtfs_file in files:
-                zipf.write(path.join(root, gtfs_file), gtfs_file)
-
-    return path.join(basedir, 'fixtures', scenario + '.zip')
+    fixture_dir = os.path.join(os.path.dirname(__file__), 'fixtures')
+    scenario_dir = os.path.join(fixture_dir, scenario)
+    return shutil.make_archive(scenario_dir, 'zip', scenario_dir)
