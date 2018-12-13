d753e35bccbf07702e00823304819dd085f501a1
Danny Whalen <daniel.r.whalen@gmail.com>
2018-12-13 11:32:14 -0800

    Use more precise type hints

--- partridge/types.py
+++ partridge/types.py
@@ -1,12 +1,12 @@
 import datetime
-from typing import FrozenSet, Iterable, List, Mapping, NewType, Union
+from typing import Dict, FrozenSet, Iterable, List, NewType, Union
 
 
-CountsByDate = Mapping[datetime.date, int]
+CountsByDate = Dict[datetime.date, int]
 Dates = List[datetime.date]
 ServiceID = NewType("ServiceID", str)
 Service = FrozenSet[ServiceID]
-ServicesByDate = Mapping[datetime.date, Service]
-DatesByService = Mapping[Service, FrozenSet[datetime.date]]
+ServicesByDate = Dict[datetime.date, Service]
+DatesByService = Dict[Service, FrozenSet[datetime.date]]
 Value = Union[str, Iterable]
-View = Mapping[str, Mapping[str, Value]]
+View = Dict[str, Dict[str, Value]]
